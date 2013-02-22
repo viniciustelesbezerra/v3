c2d3e935161471523bacf510bb7c0d8ab4fe4176
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-21 23:48:52 -0300

    Sending skelleton

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Evorav3::Application.config.secret_token = '67172de4593f2cfa440df9f1066a1828330681a71579096f84661f182dd2132cfd36bdc9a005cd0aed07c66df3232a5b4eac185b8f34763998c0dbb320fce210'
+Evorav3::Application.config.secret_token = 'a4a38765416f09cd484c2d2488f4f40d8fc08740c12c388f658c2c94f36c7fcf6650ec6004c659f147195078109db248f2ef81c18dbb5016289a4dceb5f0e225'
