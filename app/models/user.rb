bdca8e228ef0fb0893cf79b6deab9c882fc85295
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-23 01:24:33 -0300

    Sending validation to required attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   attr_protected :admin
 
+  validates_presence_of :email, :encrypted_password, :name
+
 end
